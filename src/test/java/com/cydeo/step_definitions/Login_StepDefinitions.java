fe2ac815f75ef5a5daab1db2616df236ddd588e3
gurhank <gurhankocyigit@gmail.com>
2022-03-17 10:24:13 -0500

    day 15 login.feature

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -38,5 +39,10 @@ public class Login_StepDefinitions {
     @And("user enters admin password")
     public void userEntersAdminPassword() {
         System.out.println("user enters admin password");
+    }
+
+    @Given("user is on the library login page")
+    public void userIsOnTheLibraryLoginPage() {
+        System.out.println("user is on the library login page");
     }
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -6,8 +6,9 @@ Feature: Library app login feature
 
   Accounts are: librarian, student, admin
 
-  @librarian @employee
+  @librarian @employee @wip
   Scenario: Login as librarian
+    Given user is on the library login page
     When user enters librarian username
     And user enters librarian password
     Then user should see the dashboard
