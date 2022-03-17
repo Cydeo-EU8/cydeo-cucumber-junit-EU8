d454141cd83fdd46f76807f588b4f8268993708f
gurhank <gurhankocyigit@gmail.com>
2022-03-17 11:13:22 -0500

    day 15 Hooks.java

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -5,5 +5,14 @@ In the class we will be able to pass pre- & post- conditions to
  each scenario and each step
  */
 
+import io.cucumber.java.Before;
+
 public class Hooks {
+
+    //import from io.cucumber.java not from junit
+    @Before
+    public void setupScenario(){
+        System.out.println("====Setting up browser using cucumber @Before");
+    }
+
 }
