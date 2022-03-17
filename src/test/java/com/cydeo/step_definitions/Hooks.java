234b04cefbae6cbcacd61bc5e131d86428469851
gurhank <gurhankocyigit@gmail.com>
2022-03-17 11:27:52 -0500

    day 15 Hooks.java

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -6,7 +6,9 @@ In the class we will be able to pass pre- & post- conditions to
  */
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 public class Hooks {
 
@@ -20,6 +22,16 @@ public class Hooks {
     public void teardownScenario(){
         System.out.println("====Closing browser using cucumber @After");
         System.out.println("====Scenario ended/ Take screenshot if failed!");
+    }
+
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("--------> applying setup using @BeforeStep");
+    }
+
+    @AfterStep
+    public void afterStep(){
+        System.out.println("--------> applying tearDown using @AfterStep");
     }
 
 
