e748b8ad495af5a836931f6425c2fc94ed69d7ec
gurhank <gurhankocyigit@gmail.com>
2022-03-17 11:48:50 -0500

    day 15 Hooks.java

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -13,17 +13,17 @@ import io.cucumber.java.BeforeStep;
 public class Hooks {
 
     //import from io.cucumber.java not from junit
-    @Before (order = 0)
+    @Before (order = 1)
     public void setupScenario(){
         System.out.println("====Setting up browser using cucumber @Before");
     }
 
-    @Before (value = "@login", order = 1)
+    @Before (value = "@login", order = 2)
     public void setupScenarioForLogins(){
         System.out.println("====this will only apply to scenarios with @login tag");
     }
 
-    @Before (value = "@db", order = -1)
+    @Before (value = "@db", order = 0)
     public void setupForDatabaseScenarios(){
         System.out.println("====this will only apply to scenarios with @db tag");
     }
