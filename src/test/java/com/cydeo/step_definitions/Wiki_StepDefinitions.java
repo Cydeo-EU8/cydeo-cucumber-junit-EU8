70fa8cf7862e04b3973578207f6a30db10add05a
gurhank <gurhankocyigit@gmail.com>
2022-03-18 14:41:10 -0500

    day 16 WikiSearchPage.java

--- src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
@@ -1,26 +1,38 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.WikiSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class Wiki_StepDefinitions {
+
+    WikiSearchPage wikiSearchPage = new WikiSearchPage();
 
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
         Driver.getDriver().get("https://www.wikipedia.org");
     }
+
     @When("User types {string} in the wiki search box")
     public void user_types_in_the_wiki_search_box(String string) {
 
+        wikiSearchPage.searchBox.sendKeys(string);
+
     }
+
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
+
+        wikiSearchPage.searchButton.click();
 
     }
     @Then("User sees {string} is in the wiki title")
     public void user_sees_is_in_the_wiki_title(String string) {
+        //verify actual title contains string (coming from feature file)
+        Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
 
     }
 
