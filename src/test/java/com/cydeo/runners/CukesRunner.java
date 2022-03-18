eb5987c8b3bc590e98d77163f3cf7af33233179b
gurhank <gurhankocyigit@gmail.com>
2022-03-18 13:15:48 -0500

    day 16 GoogleStepDefinitions.java

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@wip"
 )
 public class CukesRunner {
--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -13,12 +13,20 @@ public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+    @When("user types {word} and clicks enter")
+    public void user_types_and_clicks_enter2(String searchKeyword) {
+
+        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
+
+    }
+
     @When("user types {string} and clicks enter")
     public void user_types_and_clicks_enter(String searchKeyword) {
 
         googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
 
     }
+
     @Then("user sees {string} in the google title")
     public void user_sees_in_the_google_title(String string) {
 
@@ -30,14 +38,10 @@ public class GoogleStepDefinitions {
 
     }
 
-    @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
-        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
-    }
-    @Then("user sees apple in the google title")
-    public void user_sees_apple_in_the_google_title() {
+    @Then("user sees {word} in the google title")
+    public void user_sees_apple_in_the_google_title(String word) {
 
-        String expectedTitle = "apple - Google Search";
+        String expectedTitle = word+" - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
 
         //Junit assertion accepts first arg as expected, second arg as actual
