53f6f9e15f2aec0a8f23cd841202247fcf1b0128
gurhank <gurhankocyigit@gmail.com>
2022-03-18 10:09:21 -0500

    day 16 GoogleSearchPage.java

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -1,14 +1,22 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleSearchPage {
 
+    //create constructor
+    // initialize the driver instance and this class' instance using PageFactory.initElements
     public GoogleSearchPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    //We can start locating web elements using @FindBy annotation
+    @FindBy(name = "q")
+    public WebElement searchBox;
 
+    //break until 10.07am cst
 
 }
