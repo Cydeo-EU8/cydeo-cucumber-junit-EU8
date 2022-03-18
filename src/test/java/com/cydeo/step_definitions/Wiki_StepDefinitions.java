e56bf48b5aa29eedc007cc424341faa597fc9153
gurhank <gurhankocyigit@gmail.com>
2022-03-18 14:27:38 -0500

    day 16 Hooks.java take screen shot

---
+++ src/test/java/com/cydeo/step_definitions/Wiki_StepDefinitions.java
@@ -0,0 +1,26 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Wiki_StepDefinitions {
+
+    @Given("User is on Wikipedia home page")
+    public void user_is_on_wikipedia_home_page() {
+
+    }
+    @When("User types {string} in the wiki search box")
+    public void user_types_in_the_wiki_search_box(String string) {
+
+    }
+    @When("User clicks wiki search button")
+    public void user_clicks_wiki_search_button() {
+
+    }
+    @Then("User sees {string} is in the wiki title")
+    public void user_sees_is_in_the_wiki_title(String string) {
+
+    }
+
+}
--- src/test/resources/features/wikiSearch.feature
+++ src/test/resources/features/wikiSearch.feature
@@ -1,7 +1,9 @@
 Feature: Wikipedia search functionality and verifications
 
+  #WSF-45234
+
   Scenario: Wikipedia Search Functionality Title Verification
   Given User is on Wikipedia home page
-  When User types Steve Jobs in the wiki search box
+  When User types "Steve Jobs" in the wiki search box
   And User clicks wiki search button
-  Then User sees Steve Jobs is in the wiki title
+  Then User sees "Steve Jobs" is in the wiki title
