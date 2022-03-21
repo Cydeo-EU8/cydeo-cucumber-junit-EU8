6d9cf1d8cd8a05334181818a32800590a540b7e9
gurhank <gurhankocyigit@gmail.com>
2022-03-21 13:46:37 -0500

    day 17 Dropdown datatable task

--- src/test/java/com/cydeo/pages/DropdownsPage.java
+++ src/test/java/com/cydeo/pages/DropdownsPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DropdownsPage {
@@ -9,5 +11,7 @@ public class DropdownsPage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(id = "month")
+    public WebElement monthDropdown;
 
 }
