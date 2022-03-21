c5c6cc3f561008a67dafd3fd25e6276ce674d5d7
gurhank <gurhankocyigit@gmail.com>
2022-03-21 14:30:37 -0500

    day 17 Dropdown datatable task

--- src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 import com.cydeo.pages.DropdownsPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -34,21 +35,11 @@ public class DataTable_StepDefinitions {
     @Then("User should see below info in month dropdown")
     public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonths) {
 
-        Select select = new Select(dropdownsPage.monthDropdown);
+        //This method will return us the List of String of given dropdown's options
+        List<String> actualMonths = BrowserUtils.dropdownOptionsAsString(dropdownsPage.monthDropdown);
 
-        //List of all ACTUAL month <options> as a web element
-        List<WebElement> actualOptionsAsWebElement = select.getOptions();
-
-        //List of all ACTUAL month <options> as a string
-        List<String> actualOptionsAsString= new ArrayList<>();
-
-        for (WebElement each : actualOptionsAsWebElement) {
-            actualOptionsAsString.add(each.getText());
-        }
-
-        Assert.assertEquals(expectedMonths, actualOptionsAsString);
+        Assert.assertEquals(expectedMonths, actualMonths);
         //Assert will check the size of the lists first. If it is matching it will check content 1 by 1.
-
 
     }
 
