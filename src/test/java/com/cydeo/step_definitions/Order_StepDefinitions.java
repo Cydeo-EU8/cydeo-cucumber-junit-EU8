c74be2917031d3bc395ee95b532daaf355c7c6fb
gurhank <gurhankocyigit@gmail.com>
2022-03-22 11:13:46 -0500

    day 18 BrowserUtils.java

--- src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
@@ -91,22 +91,20 @@ public class Order_StepDefinitions {
     }
     @When("user enters credit card number {string}")
     public void user_enters_credit_card_number(String string) {
-
+        orderPage.cardNoInput.sendKeys(string);
     }
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
-
+        orderPage.cardExpInput.sendKeys(string);
     }
     @When("user enters process order button")
     public void user_enters_process_order_button() {
-
+        orderPage.processOrderBtn.click();
     }
     @Then("user should see {string} in first row of the web table")
     public void user_should_see_in_first_row_of_the_web_table(String string) {
 
     }
-
-    //break until 11.10am cst
 
 
 }
