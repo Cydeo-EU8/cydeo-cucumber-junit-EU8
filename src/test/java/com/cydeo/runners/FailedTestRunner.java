14b0baa47d409b7f5fbf24ffe25456e5a9d4308a
gurhank <gurhankocyigit@gmail.com>
2022-03-22 13:44:20 -0500

    day 18 FailedTestRunner.java

--- src/test/java/com/cydeo/runners/FailedTestRunner.java
+++ src/test/java/com/cydeo/runners/FailedTestRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
+        glue = "com/cydeo/step_definitions",
+        features = "@target/rerun.txt"
 )
 public class FailedTestRunner {
 }
