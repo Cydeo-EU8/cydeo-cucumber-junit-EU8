bc602f0212f5145c6aad3e583719a5b8f0d920ce
gurhank <gurhankocyigit@gmail.com>
2022-03-22 10:20:18 -0500

    day 18 BrowserUtils.java

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -93,6 +93,12 @@ for given duration
     }
 
 
+    /**
+     * This method will accept a group radio buttons as a List of WebElement.
+     * It will loop through the List, and click to the radio button with provided attributeValue
+     * @param radioButtons
+     * @param attributeValue
+     */
     public static void clickRadioButton(List<WebElement> radioButtons, String attributeValue){
 
         for (WebElement each : radioButtons) {
