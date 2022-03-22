329bd0953d92aa96e579be495e41d41a8d01c8e5
gurhank <gurhankocyigit@gmail.com>
2022-03-22 14:43:56 -0500

    day 18 pom.xml

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -14,17 +14,17 @@ import org.openqa.selenium.TakesScreenshot;
 public class Hooks {
 
     //import from io.cucumber.java not from junit
-    @Before (order = 1)
+    //@Before (order = 1)
     public void setupScenario(){
         System.out.println("====Setting up browser using cucumber @Before");
     }
 
-    @Before (value = "@login", order = 2)
+    //@Before (value = "@login", order = 2)
     public void setupScenarioForLogins(){
         System.out.println("====this will only apply to scenarios with @login tag");
     }
 
-    @Before (value = "@db", order = 0)
+    //@Before (value = "@db", order = 0)
     public void setupForDatabaseScenarios(){
         System.out.println("====this will only apply to scenarios with @db tag");
     }
@@ -51,12 +51,12 @@ public class Hooks {
         //System.out.println("====Scenario ended/ Take screenshot if failed!");
     }
 
-    @BeforeStep
+   // @BeforeStep
     public void setupStep(){
         System.out.println("--------> applying setup using @BeforeStep");
     }
 
-    @AfterStep
+    //@AfterStep
     public void afterStep(){
         System.out.println("--------> applying tearDown using @AfterStep");
     }
