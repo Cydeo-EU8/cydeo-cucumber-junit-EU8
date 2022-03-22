5abffde51d52c334456d5b4ff427afc2a57af550
gurhank <gurhankocyigit@gmail.com>
2022-03-22 14:38:34 -0500

    day 18 pom.xml

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -8,12 +8,13 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt"
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@femaleScientists or @soccerPlayers"
+        tags = "@smoke"
 )
 public class CukesRunner {
 }
