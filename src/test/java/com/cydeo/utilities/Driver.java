4144df2ed16709956dbea996e1ad77a56c27e51d
OscarSah <oscar@cybertekschool.com>
2022-06-30 22:30:58 +0300

    Selenium Grid remote case added

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -74,20 +74,7 @@ public class Driver {
                         e.printStackTrace();
                     }
                  break;
-                case "saucelab":
-                    // assign your grid server address
 
-                    try {
-                        URL url = new URL("https://oauth-sdetoscar-844c8:66e7117f-390e-4556-8105-07af96a01f7a@ondemand.eu-central-1.saucelabs.com:443/wd/hub");
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName("chrome");
-                        driverPool.set(new RemoteWebDriver(url,desiredCapabilities));
-                        driverPool.get().manage().window().maximize();
-                        driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
             }
         }
 
